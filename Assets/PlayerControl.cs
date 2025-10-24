2ed6fc49426527740247044a5d5f4b4579ec32e2
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-10-24 05:39:53 -0700

    Basic development
    Added ground and player. configured 2D top-down camera with lighting. WASD Player movement added.

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    float base_speed = 3.5f; // base speed
+    private Rigidbody player; // player rigidbody object
+    private Vector3 movement; // movement input variable
+
+    // start
+    void Start()
+    {
+        
+        player = GetComponent<Rigidbody>(); // player rigidbody component
+    }
+
+    // update
+    void Update()
+    {
+        float x_movement = Input.GetAxisRaw("Horizontal"); // horizontal movement input
+        float z_movement = Input.GetAxisRaw("Vertical"); // vertical movement input
+        movement = new Vector3(x_movement, 0f, z_movement); // movement input
+
+        
+    }
+
+    // fixed update
+    private void FixedUpdate()
+    {
+        player.MovePosition(player.position // player position movement calculation
+            + movement  
+            * base_speed 
+            * Time.fixedDeltaTime);
+    }
+}
