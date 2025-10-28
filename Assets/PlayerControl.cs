0bc125491e6313bc249ce372880676afaa3b341d
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-10-28 01:53:22 -0700

    Stamina System & Sprinting
    Added a sprint function with stamina that decreases and regenerates. Added a slider to the UI at the top inbetween the score and highscore. Lastly, made the white lines in the road dotted lines like real roads.

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -9,29 +10,57 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody player; // player rigidbody object
     private Vector3 movement; // movement input variable
 
+    public float maxStamina = 100f;
+    public float currentStamina;
+    public float sprintSpeed = 8f;
+    public float staminaDrain = 20f;
+    public float staminaRegen = 15f;
+    public Slider staminaBar;
+
+    private Vector3 velocity;
+
     // start
     void Start()
     {
         
         player = GetComponent<Rigidbody>(); // player rigidbody component
+        currentStamina = maxStamina;
+        if (staminaBar != null)
+        {
+            staminaBar.value = currentStamina;
+        }
     }
 
     // update
     void Update()
     {
-        float x_movement = Input.GetAxisRaw("Horizontal"); // horizontal movement input
-        float z_movement = Input.GetAxisRaw("Vertical"); // vertical movement input
-        movement = new Vector3(x_movement, 0f, z_movement); // movement input
 
-        
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        bool sprinting = Input.GetKey(KeyCode.LeftShift) && currentStamina > 0f && direction.magnitude > 0f;
+        float speed = sprinting ? sprintSpeed : base_speed;
+
+        velocity = direction * speed;
+
+        if (sprinting)
+        {
+            currentStamina = Mathf.Max(currentStamina - staminaDrain * Time.deltaTime, 0f);
+        }
+        else if(direction.magnitude == 0f)
+        {
+            currentStamina = Mathf.Min(currentStamina + staminaRegen * Time.deltaTime, maxStamina);
+        }
+
+        if (staminaBar != null) 
+        {
+            staminaBar.value = currentStamina;
+        }
     }
 
     // fixed update
     private void FixedUpdate()
     {
-        player.MovePosition(player.position // player position movement calculation per frame
-            + movement  
-            * base_speed 
+        player.MovePosition(player.position // player position movement calculation per frame  
+            + velocity 
             * Time.fixedDeltaTime);
     }
 
